6e9514c0ed8f0d3ea634393489d3eca205bfa6fa
Yevhen Podoliaka <zanp85740@gmail.com>
2023-03-07 10:45:10 +0200

    moved from the old repo

---
+++ src/controllers/auth/login.ts
@@ -0,0 +1,34 @@
+import {Request, Response} from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { User,IUser} from "../../models/User";
+import { RequestError } from "../../helpers";
+
+dotenv.config();
+
+const {SECRET_KEY = ""} = process.env;
+
+const login = async (req: Request, res: Response): Promise<void> | never => {
+    const {email, password} = req.body;
+    const user: IUser | null = await User.findOne({email});
+    if(!user){
+        throw RequestError(401, "Email not found");
+    }
+      const passwordCompare = await bcrypt.compare(password, user.password)
+    if(!passwordCompare) {
+        throw RequestError(401, "Password wrong");
+    }
+    const payload = {
+        id: user._id,
+    }
+ 
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "24h"});
+    await User.findByIdAndUpdate(user._id, {token});
+    res.json({
+        email: user.email,
+        token
+    })
+}
+
+export default login;
---
+++ src/controllers/auth/register.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { User, IUser } from "../../models/User";
+import { RequestError } from "../../helpers";
+
+dotenv.config();
+
+const { SECRET_KEY = "" } = process.env;
+
+const register = async (req: Request, res: Response): Promise<void> | never => {
+  const { email, password, name } = req.body;
+  const user: IUser | null = await User.findOne({ email });
+  if (user) {
+    throw RequestError(409, `User with ${email} is already exist`);
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser: IUser = await User.create({
+    name,
+    email,
+    password: hashPassword,
+  });
+  const payload = {
+    id: newUser._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  await User.findByIdAndUpdate(newUser._id, { token });
+  res.status(201).json({
+    name: newUser.email,
+    token,
+  });
+};
+
+export default register;
